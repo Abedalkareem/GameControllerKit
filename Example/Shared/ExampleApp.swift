b0ed04bb5e667ebcef3b9ad623ffbadb9dd0a32d
abedalkareem omreyh <abedalkareem.omreyh@yahoo.com>
2022-07-30 08:19:17 +0400

    Add example folder

---
+++ Example/Shared/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Shared
+//
+//  Created by abedalkareem omreyh on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
